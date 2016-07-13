a03655658a8298145ba32f3e50802597559ce263
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2016-07-13 07:23:03 -0400

    Final commit

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton!) {
+        if(howManyTapsTxtValidation()){
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            toggleView(false)
+            
+            updateTapsLabel()
+        }
+    }
+    
+    func howManyTapsTxtValidation() -> Bool{
+        return howManyTapsTxt != nil && howManyTapsTxt.text != "" && Int(howManyTapsTxt.text!) != nil
+    }
+    
+    @IBAction func onCoinButtonPressed(sender: UIButton) {
+        currentTaps += 1
+        updateTapsLabel()
+        
+        if(isGameOver()){
+            restartGame()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapper = UITapGestureRecognizer(target: view, action:#selector(UIView.endEditing))
+        tapper.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapper)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTapsLabel() {
+        tapsLabel.text = "\(currentTaps) Taps"
     }
-
-
+    
+    func isGameOver() -> Bool {
+        return currentTaps >= maxTaps
+    }
+    
+    func restartGame(){
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        toggleView(true)
+    }
+    
+    func toggleView(showStartScreen: Bool) {
+        logoImage.hidden = !showStartScreen
+        howManyTapsTxt.hidden = !showStartScreen
+        playButton.hidden = !showStartScreen
+        
+        coinButton.hidden = showStartScreen
+        tapsLabel.hidden = showStartScreen
+    }
 }
 
